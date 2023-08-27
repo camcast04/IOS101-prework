e85be248e8b993bbea40d193ef8d1cb360da8c63
camcast04 <ccast049@fiu.edu>
2023-08-27 19:05:23 -0400

    IOS prework

--- IOS101-prework/ViewController.swift
+++ IOS101-prework/ViewController.swift
@@ -14,6 +14,31 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBOutlet weak var name: UILabel!
+    
+    @IBOutlet weak var university: UILabel!
+    
+    @IBOutlet weak var job: UILabel!
+    
+    @IBAction func changeBackgroundColor(_ sender: UIButton) {
+        let randomColor = changeColor()
+        view.backgroundColor = randomColor
+    }
+    
+    @IBAction func changeTextColor(_ sender: UIButton) {
+        let randomColor = changeColor()
+        name.textColor = randomColor
+        university.textColor = randomColor
+        job.textColor = randomColor
+    }
+    func changeColor() -> UIColor{
 
+           let red = CGFloat.random(in: 0...1)
+           let green = CGFloat.random(in: 0...1)
+           let blue = CGFloat.random(in: 0...1)
+
+           return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+       }
+    
 }
 
